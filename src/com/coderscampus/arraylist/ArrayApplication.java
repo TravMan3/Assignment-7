0bbbdd7a156d59072177c696de70629b2f7a3aa2
Travis C. <140869835+TravMan3@users.noreply.github.com>
2024-10-07 18:19:53 -0600

    10/7/24 Push

--- src/com/coderscampus/arraylist/ArrayApplication.java
+++ src/com/coderscampus/arraylist/ArrayApplication.java
@@ -1,38 +1,45 @@
 package com.coderscampus.arraylist;
 
+import java.util.Arrays;
+
 public class ArrayApplication {
 
 	public static void main(String[] args) {
-		CustomList<String> items = new CustomArrayList<>();
+		CustomList<String> sut = new CustomArrayList<>();
 		CustomList<Integer> numItems = new CustomArrayList<>();
 		
-		items.add("Red");
-		items.add("Yellow");
-		items.add("Blue");
-		items.add("Green");
-		items.add("Purple");
-		items.add("Orange");
-		items.add("Black");
-		items.add("White");
-		items.add("Brown");
-		items.add("Grey");
-		items.add("Pink");
-		items.add("Coral");
-		items.add("Gold");
-		items.add("Plum");
-		items.add("Orchid");
-		items.add("Indigo");
+		sut.add("Red");
+		sut.add("Yellow");
+		sut.add("Blue");
+		sut.add("Green");
+		sut.add("Purple");
+		sut.add("Orange");
+		sut.add("Black");
+		sut.add("White");
+		sut.add("Brown");
+		sut.add("Grey");
+		sut.add("Pink");
+		sut.add("Coral");
+		sut.add("Gold");
+		sut.add("Plum");
+		sut.add("Orchid");
+		sut.add("Indigo");
+		System.out.println(sut);
+		sut.add(10, "Honey");
+		sut.remove(5);
+		System.out.println(sut);
+		System.out.println(sut);
 		System.out.println("--------------------------");
-		for(int i = 0; i< 161; i++) {
+		for(int i = 0; i< 10; i++) {
 			numItems.add(i);
 		}
 		System.out.println("--------------------------");
-		System.out.println(items.getSize());
-		System.out.println(items.get(10));
+		System.out.println(sut.getSize());
+		System.out.println(sut.get(10));
 		System.out.println("--------------------------");
 		System.out.println(numItems.getSize());
-		System.out.println(numItems.get(28));
-		
+		System.out.println(numItems.get(8));
+		System.out.println(numItems);
 		
 	}
 
--- src/com/coderscampus/arraylist/CustomArrayList.java
+++ src/com/coderscampus/arraylist/CustomArrayList.java
@@ -1,21 +1,16 @@
 package com.coderscampus.arraylist;
 
+import java.util.Arrays;
+
 public class CustomArrayList<T> implements CustomList<T> {
 	Object[] items = new Object[10];
 	Integer arrayCount = 0;
-	
+
 	@Override
 	public boolean add(T item) {
-		if (arrayCount == items.length) {
-			Object[] biggerItems = new Object[items.length * 2];
-			System.arraycopy(items,0, biggerItems, 0, items.length);
-			biggerItems[arrayCount] = item;	
-			items = biggerItems;
-		} else {
-			items[arrayCount] = item;
-		}
+		resizeArray(item);
+		items[arrayCount] = item;
 		arrayCount++;
-		System.out.println(item);
 		return true;
 	}
 
@@ -32,14 +27,54 @@ public class CustomArrayList<T> implements CustomList<T> {
 
 	@Override
 	public boolean add(int index, T item) throws IndexOutOfBoundsException {
-		// TODO Auto-generated method stub
-		return false;
+		checkForValidIndex(index);
+		resizeArray(item);
+		for (int i = arrayCount; i > index; i--) {
+			items[i] = items[i - 1];
+		}
+
+		items[index] = item;
+		arrayCount++;
+		return true;
 	}
 
 	@Override
 	public T remove(int index) throws IndexOutOfBoundsException {
-		// TODO Auto-generated method stub
-		return null;
+		checkForValidIndex(index);
+
+		@SuppressWarnings("unchecked")
+		T itemToRemove = (T) items[index];
+
+		items[index] = null;
+		for (int i = index; i < arrayCount -1; i++) {
+			items[i] = items[i + 1];
+		}
+		items[arrayCount - 1] = null;
+		arrayCount--;
+		return itemToRemove;
+		
 	}
+
+	public void resizeArray(T item) {
+		if (arrayCount == items.length) {
+			Object[] biggerItems = new Object[items.length * 2];
+			System.arraycopy(items, 0, biggerItems, 0, items.length);
+			biggerItems[arrayCount] = item;
+			items = biggerItems;
+		}
+	}
+
+	public void checkForValidIndex(int index) {
+		if (index < 0 || index > arrayCount) {
+			throw new IndexOutOfBoundsException("Index: " + index + " is out of bounds.");
+		}
+	}
+
+	@Override
+	public String toString() {
+		return "CustomArrayList [items=" + Arrays.toString(items) + ", ArrayCount=" + arrayCount + "]";
+	}
+	
+
 	
 }
