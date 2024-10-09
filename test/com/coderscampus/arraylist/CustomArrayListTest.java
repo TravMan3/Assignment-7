21c511136eb54e7adec36e2f7a595ca093ed5989
Travis C. <140869835+TravMan3@users.noreply.github.com>
2024-10-09 11:19:36 -0600

    10.9.24

--- test/com/coderscampus/arraylist/CustomArrayListTest.java
+++ test/com/coderscampus/arraylist/CustomArrayListTest.java
@@ -5,10 +5,15 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 class CustomArrayListTest {
-
+	
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void should_add_one_item_to_the_list() {
+		
+		CustomList<Integer> customList = new CustomArrayList<>();
+		
+		customList.add(10);
+		assertEquals(10, customList.get(0));
+		assertEquals(1, customList.getSize());
 	}
 
 }
