a0e1659f27369a9f12eea883902aa498fa19cde6
Travis C. <140869835+TravMan3@users.noreply.github.com>
2024-10-10 14:04:54 -0600

    10.10.24

--- test/com/coderscampus/arraylist/CustomArrayListTest.java
+++ test/com/coderscampus/arraylist/CustomArrayListTest.java
@@ -1,5 +1,6 @@
 package com.coderscampus.arraylist;
 
+import static org.junit.Assert.assertThrows;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
@@ -50,5 +51,63 @@ class CustomArrayListTest {
 		sut.add(7, 2963);
 		assertEquals(2963, sut.get(7));
 	}
-
+	
+	 @Test
+	 void should_throw_index_out_of_bounds_when_adding_index_out_of_range() {
+        CustomList<Integer> sut = new CustomArrayList<>();
+        for (int i = 0; i < 11; i++) {
+            sut.add(i);
+        }
+        assertThrows(IndexOutOfBoundsException.class, () -> sut.add(29, 152));
+	    }
+	 
+	 @Test
+	 void should_throw_index_out_of_bounds_when_removing_index_out_of_range() {
+        CustomList<Integer> sut = new CustomArrayList<>();
+        for (int i = 0; i < 11; i++) {
+            sut.add(i);
+        }
+        assertThrows(IndexOutOfBoundsException.class, () -> sut.remove(23));
+	    }
+	 	
+	 @Test
+	    void should_get_size() {
+	        CustomList<Integer> sut = new CustomArrayList<>();
+	        for (int i = 0; i < 11; i++) {
+	            sut.add(i);
+	        }
+	        assertEquals(11, sut.getSize());
+	 }
+	 
+	 @Test
+	    void should_get_index() {
+	        CustomList<Integer> sut = new CustomArrayList<>();
+	        for (int i = 0; i < 11; i++) {
+	            sut.add(i);
+	        }
+	        assertEquals(5, sut.get(5));
+	 }   
+	 
+	 @Test
+	    void should_resize_array() {
+	        CustomList<Integer> sut = new CustomArrayList<>();
+	        for (int i = 0; i < 10; i++) {
+	            sut.add(i);
+	        }
+	        sut.add(12);
+	        assertEquals(12, sut.get(10));
+	        assertEquals(11, sut.getSize());
+	 } 
+	 
+	 @Test
+	    void should_throw_exection_if_index_out_of_range() {
+	        CustomList<Integer> sut = new CustomArrayList<>();
+	        for (int i = 0; i < 10; i++) {
+	            sut.add(i);
+	        }
+	        assertThrows(IndexOutOfBoundsException.class, () -> sut.get(-1));
+	        assertThrows(IndexOutOfBoundsException.class, () -> sut.get(11));
+	 } 
+	 
 }
+
